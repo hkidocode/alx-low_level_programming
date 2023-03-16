18b862a8ed94c3acaec57cc760ad77fbf3d8af66
hkidocode <kadouri.freelancer124@gmail.com>
2023-03-16 15:58:17 -0700

    update code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main : entry point of program
+ * puts : prints text
+ * return (0) : stop the program
+ *
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle.\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
