c0753dca493033a9a602d3d9df9f7a35e3672bbf
hkidocode <kadouri.freelancer124@gmail.com>
2023-03-16 16:47:08 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+/**
+ * main - program that prints size of data types usinf sizeof function
+ * Return: 0 (success)
+*/
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of a int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)", sizeof(float));
+return (0);
+}
