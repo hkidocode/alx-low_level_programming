0754d504cd4de0cca2d160e5ba7735d183cd81f8
hkidocode <kadouri.freelancer124@gmail.com>
2023-03-18 13:32:00 -0700

    print all numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program that convert alphabet to lowercase except q and e
+ * Return: 0 (success)
+*/
+int main(void)
+{
+
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(48 + i);
+}
+
+putchar('\n');
+
+return (0);
+}
