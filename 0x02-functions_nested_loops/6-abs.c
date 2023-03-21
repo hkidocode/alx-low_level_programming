0887b4b3f14f322d0dfb13c4cc271620edaa8fbe
hkidocode <kadouri.freelancer124@gmail.com>
2023-03-21 13:54:30 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - function that prints the absolute value of a number
+ * Return: absolute value
+*/
+int _abs(int num)
+{
+
+if (num < 0)
+{
+return (-num);
+}
+else
+{
+return (num);
+}
+}
+
+
