f6f7d2b42be295e4581bd41127b46cf712454d1c
hkidocode <kadouri.freelancer124@gmail.com>
2023-03-21 16:02:06 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,14 +13,24 @@ for (i = 0; i < 10; i++)
 for (j = 0; j < 10; j++)
 {
 int product = i * j;
-_putchar(product + '0');
+int tens = product / 10;
+int ones = product % 10;
+if (tens != 0)
+{
+putchar(product / 10 + '0');
+}
+else
+{
+putchar(' ');
+}
+putchar(product % 10 + '0');
 if (j != 9)
 {
-_putchar(',');
-_putchar(' ');
+putchar(',');
+putchar(' ');
 }
 }
-_putchar('\n');
+putchar('\n');
 }
 }
 
