12afbe3ba83e5196e7fb248b8dbdb1b9c762ef5a
hkidocode <kadouri.freelancer124@gmail.com>
2023-03-21 12:10:13 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - function that prints alphabet in lowercase
+ * Return: nothing
+*/
+void print_alphabet(void)
+{
+
+char c;
+for (c = 'A'; c < 'Z'; c++)
+{
+_putchar(c + 32);
+}
+
+_putchar('\n');
+
+}
