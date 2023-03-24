f1e1dbc1adf113959dbdc13d7127f85721b6a04e
hkidocode <kadouri.freelancer124@gmail.com>
2023-03-24 16:49:32 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,10 +10,11 @@ void print_most_numbers(void)
 int i;
 for (i = 0; i < 10; i++)
 {
-if (i != 2 || i != 4)
+if (i == 2 || i == 4)
 {
-_putchar(i + 48);
+continue;
 }
+_putchar(i + 48);
 
 }
 _putchar('\n');
